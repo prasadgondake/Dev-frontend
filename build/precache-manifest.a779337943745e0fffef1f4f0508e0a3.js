4f73ec94e03e4481777fb3b864aeb507a3696d1b
Prasad Gondake <prasadgondake0910@gmail.com>
2023-08-22 16:22:01 +0530

    Updated commits

---
+++ build/precache-manifest.a779337943745e0fffef1f4f0508e0a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0701f1d3d09bc2d3fe63",
+    "url": "/static/css/main.68680ff4.chunk.css"
+  },
+  {
+    "revision": "0701f1d3d09bc2d3fe63",
+    "url": "/static/js/main.89d00db3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b4b111fbfa160397ad6",
+    "url": "/static/js/2.7364ef56.chunk.js"
+  },
+  {
+    "revision": "1af68cbeb63f40d4262f43779e113161",
+    "url": "/static/media/spinner.1af68cbe.gif"
+  },
+  {
+    "revision": "f71ad76563e6f81a8991fae190a2bbac",
+    "url": "/static/media/showcase1.f71ad765.png"
+  },
+  {
+    "revision": "68681f1243f648c12db5675175068cdf",
+    "url": "/index.html"
+  }
+];
